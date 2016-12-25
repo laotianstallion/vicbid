707138a112da61f91f96ccdba5c10ad869a4eb2b
laotianstallion <rudy@zipnosis.com>
2016-12-25 10:45:37 -0600

    Added login component

---
+++ app/components/header/component.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var ctrl = [ function () {
+  return;
+
+}];
+
+angular.module('app.component').component('vaHeader', {
+  controller: ctrl,
+  templateUrl: "components/header/index.html"
+});
---
+++ app/components/login/component.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var ctrl = [ function () {
+  return;
+
+}];
+
+angular.module('app.component').component('vaLogin', {
+  controller: ctrl,
+  templateUrl: "components/login/index.html"
+});
